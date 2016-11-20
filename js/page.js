b34e12e25fdccea564df02472f54549e221fbdf2
Dmytro Chasovskyi <dmitriy.chasovskoy@gmail.com>
2016-11-20 14:38:10 +0200

    Fix translations show-up and follow-up gamification

--- js/page.js
+++ js/page.js
@@ -14,82 +14,76 @@ function CreateImagePath(company) {
   }
 }
 
+function CleanTransaltionTitle() {
+  $("#translation-title").empty();
+  $("div[class='space5percent']").remove();
+}
+
+function CleanTranslationDivs() {
+  $("#translation-choice").empty();
+  $("#translation-choice").remove(".space3percent");
+}
+
+function CleanTranslationAll() {
+  CleanTransaltionTitle();
+  CleanTranslationDivs();
+}
+
 function CreateTranslationRow(image_path, traslation_text) {
-  // $(document.createElement('img'))
-  var translation_div = $(document.createElement('div')).addClass("row"),
+  var translation_div = document.createElement('div'),
       image_div = $(document.createElement('div')).addClass("wrapper col-lg-2 col-md-2 col-sm-2 col-xs-2"),
       text_div = $(document.createElement('div')).addClass("col-lg-10 col-md-10 col-sm-10 col-xs-10"),
       image = $(document.createElement('img')).addClass("img-responsive icon-max-size pull-right");
 
-  image.attr("src", image_path);
-  console.log("image", image);
-  text_div.text(traslation_text);
-  console.log("text_div", text_div);
-  image.appendTo(image_div);
-  //image_div.append(image);
-  console.log("image_div", image_div);
-  translation_div.append(image_div);
-  console.log("translation_div", translation_div);
-  translation_div.append(text_div);
-  console.log("translation_div", translation_div);
+  // Add image + translation
+  $(translation_div).addClass("row");
+  $(image).attr("src", image_path);
+  $(image).attr("onclick", "ShowTranslatorsBasedOnTranslation()");
+  $(text_div).text(traslation_text);
+  $(image).appendTo(image_div);
+  $(translation_div).append(image_div);
+  $(translation_div).append(text_div);
+  $("#translation-choice").append(translation_div);
 
-  return translation_div;
+  // Add space
+  var space_div = document.createElement('div');
+  $(space_div).addClass("space3percent");
+  $("#translation-choice").append(space_div);
+
+  return null;
+}
+
+function CreateTranslationTitle(translation_title) {
+  CleanTransaltionTitle();
+  $("#translation-title").text(translation_title);
+  var space_div = document.createElement('div');
+  $(space_div).addClass("space5percent");
+  $($("#translation-title").parent()).append(space_div);
 }
 
 function ShowTranslation(content = [{translator: "google", translation: "hello"},
                                     {translator: "google", translation: "goodb"},
                                     {translator: "google", translation: "oouch"}], translation_title = "Palun vali kõige parim tõlge:") {
-    /*alert("ShowTranslation");*/
-    $("#translation-title").text(translation_title);
-    for(var index in content) {
-      var image_path = CreateImagePath(content[index].translator);
-      console.log("image_path", image_path);
-      var translation_div = CreateTranslationRow(image_path, content[index].translation);
-      $("#translation-choice").append(translation_div);
-      console.log("tarnsaltion_div", translation_div);
-      console.log("index", index);
-      console.log("content[index].translator", content[index].translator);
-      console.log("content[index].translation", content[index].translation);
-      //console.error(element);
-    }
+  CleanTranslationAll();
+  CreateTranslationTitle(translation_title);
+
+  for(var index in content) {
+    var image_path = CreateImagePath(content[index].translator);
+    console.log("image_path", image_path);
+    CreateTranslationRow(image_path, content[index].translation);
+  }
 };
 
-/*
-function ShowTranslatorsBasedOnTranslation() {
-  //$("#translation-title").empty();
-  //$("#translation-choice").empty();
+function ShowTranslatorsBasedOnTranslation(
+  content = [{translator: "google", translation: "hello"},
+             {translator: "microsoft", translation: "goodb"},
+             {translator: "ut", translation: "oouch"}]) {
 
-  $("#companies-translators").html('
-    <div class="row"> \
-      <div class="wrapper col-lg-2 col-md-2 col-sm-2 col-xs-2"> \
-          <img class="img-responsive icon-max-size" src="image/Microsoft_GREY.png"/> \
-      </div> \
-      <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10"> \
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-      </div> \
-    </div> \
-    \
-    <div class="space10px"></div> \
-    \
-    <div class="row"> \
-      <div class="wrapper col-lg-2 col-md-2 col-sm-2 col-xs-2"> \
-          <img class="img-responsive icon-max-size" src="image/TU_GREY.png"/> \
-      </div> \
-      <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10"> \
-        Lorem dolor sit amet ipsum, consectetur adipiscing. \
-      </div> \
-    </div> \
-    \
-    <div class="space10px"></div> \
-    \
-    <div class="row"> \
-      <div class="wrapper col-lg-2 col-md-2 col-sm-2 col-xs-2"> \
-          <img class="img-responsive icon-max-size" src="image/Google_GREY.png"/> \
-      </div> \
-      <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10"> \
-        Lorem ipsum dolor sit amet, adipiscing consectetur elit. \
-      </div> \
-    </div>'
-  );
+  CleanTranslationAll();
+
+  for(var index in content) {
+    var image_path = CreateImagePath(content[index].translator);
+    console.log("image_path", image_path);
+    CreateTranslationRow(image_path, content[index].translation);
+  }
 };
-*/
