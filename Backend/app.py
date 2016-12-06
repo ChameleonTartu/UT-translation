c48880d56d4416ba7985beb3a5a57989a3cb38a0
Dmytro Chasovskyi <dmitriy.chasovskoy@gmail.com>
2016-12-06 15:01:33 +0200

    Add google translator

--- Backend/app.py
+++ Backend/app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template
-from translators.bing import bing_translation
+from translators.microsoft import microsoft_translation
+from translators.google import google_translation
 
 app = Flask(__name__)
 
 
+# TODO
 @app.route('/')
 def main_page():
-    translate = bing_translation("Tere")
-    print(translate)
     return "Hello"
-        #render_template('index_.html')
 
 
+# TODO
 @app.route('/about')
 def about_page():
     return "About"
 
 
+# TODO
 @app.route('/contacts')
 def contacts_page():
     return "Contacts"
 
 
+# TODO: remove debug mode in production
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Backend/translators/google.py
@@ -0,0 +1,16 @@
+import goslate
+
+
+def google_translation(text, translate_from='et', translate_to='en'):
+    translator = goslate.Goslate()
+    translation = translator.translate(text, source_language=translate_from, target_language=translate_to)
+    return translation
+
+
+def main():
+    print(google_translation("Tere hommikust"))
+    return None
+
+
+if __name__ == "__main__":
+    main()
