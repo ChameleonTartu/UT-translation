d0412f406634c004ddd82c4a3c4a23d834915a9f
Dmytro Chasovskyi <dmitriy.chasovskoy@gmail.com>
2016-12-07 15:47:04 +0200

    Add database
    - Add database structure
    - Add .gitignore

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+
+from flask import Flask
+from celery import Celery
 from translators.microsoft import microsoft_translation
 from translators.google import google_translation
+from db import insert
+
+import time
+
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 app = Flask(__name__)
 
@@ -8,6 +22,34 @@ app = Flask(__name__)
 # TODO
 @app.route('/')
 def main_page():
+    source_text = u"Heakene küll, oleks see siis olnud Jimmy. Ühes Ameerika telekanalis on selline naljamees, Jimmy Fallon. Räägitakse, et on terane poiss. Tema sõnade peale oleks Trump ehk naernudki. On ju naljakas, kui koomik üliriigi liidrile head nõu annab."
+
+    # TODO refactor
+    translation_microsoft = ""
+    try:
+        begin = time.time()
+        # Hidden credentials:
+        translation_microsoft = microsoft_translation(source_text, client_id="****", client_secret="****")
+        end = time.time()
+        print("Microsoft", end - begin)
+    except:
+        pass
+
+    # TODO refactor
+    translation_google = ""
+    try:
+        begin = time.time()
+        translation_google = google_translation(source_text)
+        end = time.time()
+        print("Google", end - begin)
+    except:
+        pass
+
+    translation_ut = ""
+
+    # Insert Query in DB
+    insert(source_text, translation_microsoft, translation_google, translation_ut)
+
     return "Hello"
 
 
